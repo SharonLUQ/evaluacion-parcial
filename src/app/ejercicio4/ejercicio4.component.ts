c76362a22774a5c97ecf648b70c04c5554543764
Sharon <blackmanuq@gmail.com>
2024-11-17 10:55:05 -0500

    ejercicio4 finalizado

--- src/app/ejercicio4/ejercicio4.component.ts
+++ src/app/ejercicio4/ejercicio4.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ejercicio4',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ejercicio4.component.html',
   styleUrl: './ejercicio4.component.css'
 })
 export class Ejercicio4Component {
+  
+  resultados = { numeros: [] as number[], cuadrados: [] as number[], mitades: [] as number[] };
 
+  calcular() {
+    const numeros = [];
+    const cuadrados = [];
+    const mitades = [];
+
+    for (let i = 15; i <= 70; i++) {
+      numeros.push(i);
+      cuadrados.push(i ** 2);
+      mitades.push(i / 2);
+    }
+
+    this.resultados = { numeros, cuadrados, mitades };
+  }
 }
