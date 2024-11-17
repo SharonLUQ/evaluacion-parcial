da4c4f1a6022c70eacbbfe5d83a0cd692fa04f89
Sharon <blackmanuq@gmail.com>
2024-11-17 10:29:40 -0500

    ejercicio3 finalizado

--- src/app/ejercicio3/ejercicio3.component.ts
+++ src/app/ejercicio3/ejercicio3.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ejercicio3',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ejercicio3.component.html',
   styleUrl: './ejercicio3.component.css'
 })
 export class Ejercicio3Component {
 
+  materias: number = 0;
+  costoPorMateria: number = 520;
+  descuento: number = 0;
+  total: number = 0;
+  resultadoVisible: boolean = false;
+
+  calcularMatricula() {
+    if (this.materias > 5) {
+      this.descuento = 0.10;
+    } else {
+      this.descuento = 0;
+    }
+    this.total = this.materias * this.costoPorMateria * (1 - this.descuento);
+    this.resultadoVisible = true;
+  }
+
+  actualizarMaterias(value: string) {
+    this.materias = +value;
+  }
 }
